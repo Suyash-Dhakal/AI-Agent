6c1b9be57801f6b12de1b7d0905946dde4a6ea8c
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-01-02 14:38:42 +0545

    feat: add Azure OpenAI embeddings and PDF query functionality

---
+++ services/getEmbeddings.service.js
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { CharacterTextSplitter } from 'langchain/text_splitter';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { AzureOpenAIEmbeddings } from '@langchain/openai'; // Import AzureOpenAIEmbeddings
+
+const question = process.argv[2] || 'hi';
+
+// Your custom model endpoint and API key for Azure
+const embeddingApiUrl = process.env.TEXT_EMBEDDING_API_ENDPOINT;
+const embeddingApiKey = process.env.TEXT_EMBEDDING_API_KEY;
+
+const embeddings = new AzureOpenAIEmbeddings({
+  azureOpenAIApiKey:process.env.TEXT_EMBEDDING_API_KEY , // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
+  azureOpenAIApiInstanceName:process.env.INSTANCE,
+  azureOpenAIApiEmbeddingsDeploymentName: process.env.DEPLOYMENT,
+  azureOpenAIApiVersion: process.env.VERSION, // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
+});
+
+const createStore = async (docs) => {
+  // Create and return the MemoryVectorStore using the embeddings
+  return MemoryVectorStore.fromDocuments(docs, embeddings);
+};
+
+const docsFromPDF = async () => {
+  const loader = new PDFLoader('Flutter-Proposal.pdf');
+  const splitter = new CharacterTextSplitter({
+    separator: '. ',
+    chunkSize: 2500,
+    chunkOverlap: 200,
+  });
+  // Load the document
+  const docs = await loader.load();
+
+  // Split the document
+  return splitter.splitDocuments(docs);
+};
+
+const loadStore = async () => {
+  const pdfDocs = await docsFromPDF();
+  return createStore([...pdfDocs]);  // Pass pdfDocs as an array to createStore
+};
+
+
+
+const query =async()=>{
+  const store=await loadStore();
+  const results=await store.similaritySearch(question,1);
+  console.log(results);
+  
+}
+
+
+query();
---
+++ services/llmResponse.service.js
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+const apiKey=process.env.OPENAI_API_KEY;
+const apiUrl=process.env.OPENAI_ENDPOINT;
+
+export default async function getResponse(history,message){
+    try {
+        
+        const response=await fetch(apiUrl,{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'api-key':apiKey
+            },
+            body:JSON.stringify({
+                model:"gpt-4-32k",
+                messages:[
+                    ...history,message
+                ]
+            }),
+        });
+        let res;
+        if (response.ok) {
+           const jsonResponse=await response.json();
+           res=jsonResponse?.choices[0]?.message;
+           return res;
+        
+        // return response?.choices[0]?.message;
+        }
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
